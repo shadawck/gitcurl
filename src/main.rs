d11a694aff231060666d3deb318b63564f0c1f0e
shadawck <shadawck@protonmail.com>
2022-09-23 12:43:03 +0200

    feat: basic gitlab support
    Add basic gitlab.com support
    Add basic on-premise gitlab support: doesn't work on on-premise gitlab with subgroup

--- src/main.rs
+++ src/main.rs
@@ -21,15 +21,18 @@
 
 use std::fs::{self, File};
 use std::io::{Cursor, Read, Write};
-use std::panic;
+use std::panic::{self};
 
 use clap::{crate_version, value_parser, App, Arg};
 use curl::easy::{Easy2, Handler, WriteError};
 use fs_extra::dir::{move_dir, CopyOptions};
 
 // Helper Type
+type HostName = str;
 type UserName = str;
 type RepoName = str;
+
+static GITHUB_HOST: &str = "github";
 
 struct CollectorMemFile(Vec<u8>);
 
@@ -40,22 +43,13 @@ impl Handler for CollectorMemFile {
     }
 }
 
-/// A gitlab repo is hosted on gitlab.com or self-hosted.
-#[allow(unused)]
-fn build_gitlab_url(dns_name: &str, user_name: &str, repo_name: &str, branch_name: &str) -> String {
-    format!(
-        "https://{}/{}/{}/-/master/{}-{}.zip",
-        dns_name, user_name, repo_name, repo_name, branch_name
-    )
-}
-
 #[derive(Debug)]
 struct StringDeserialize(String);
 
 #[derive(Clone, Debug)]
 struct Git<'a> {
     //url: &'a str,
-    zip_url: String,
+    git_zip_url: String,
     //user_name: &'a str,
     repo_name: &'a str,
     branch_name: &'a str,
@@ -69,6 +63,8 @@ fn check_split_url_length(url: &Vec<&str>, lenght: usize) {
     if url.len() != lenght {
         panic!()
     }
+
+    let _ = panic::take_hook();
 }
 
 fn handle_git_extenstion(repo_name: &str) -> &str {
@@ -80,11 +76,12 @@ fn handle_git_extenstion(repo_name: &str) -> &str {
     }
 }
 
-fn check_repo_data(user_name: &str, repo_name: &str) {
+fn check_repo_data(host_name: &HostName, user_name: &UserName, repo_name: &RepoName) {
     panic::set_hook(Box::new(|_| {
         println!("Can't get user_namename and repository name");
     }));
 
+    assert_ne!(host_name, "");
     assert_ne!(user_name, "");
     assert_ne!(repo_name, "");
 
@@ -93,7 +90,7 @@ fn check_repo_data(user_name: &str, repo_name: &str) {
 
 fn check_url_availability(response_code: u32) {
     panic::set_hook(Box::new(|_| {
-        println!("This branch name doesn't exist or repo can't be fetched");
+        println!("This repo/branch doesn't exist or repo can't be fetched");
     }));
 
     assert_eq!(response_code, 200);
@@ -110,67 +107,122 @@ fn handle_zip_extension(file_name: &str) -> String {
 }
 
 impl<'a> Git<'a> {
-    fn deserialize_input_url(url: &str) -> (&str, &str) {
+    fn deserialize_input_url(url: &str) -> (&HostName, &UserName, &RepoName) {
         if url.starts_with("http") {
             let url_split: Vec<&str> = url.split("://").collect();
             let repo_url: Vec<&str> = url_split[1].split_terminator('/').collect();
 
             check_split_url_length(&repo_url, 3);
 
+            let host_name: &HostName = repo_url[0];
             let user_name: &UserName = repo_url[1];
             let repo_name: &RepoName = handle_git_extenstion(repo_url[2]);
 
-            (user_name, repo_name)
-        } else if url.starts_with("github.com") {
+            (host_name, user_name, repo_name)
+        } else if !url.starts_with("http") && !url.contains(':') {
             let repo_url: Vec<&str> = url.split_terminator('/').collect();
             check_split_url_length(&repo_url, 3);
 
+            let host_name: &HostName = repo_url[0];
             let user_name: &UserName = repo_url[1];
             let repo_name: &RepoName = handle_git_extenstion(repo_url[2]);
 
-            (user_name, repo_name)
+            (host_name, user_name, repo_name)
         } else if !url.starts_with("http") && url.contains(':') {
             let repo_data: Vec<&str> = url.split(':').collect();
-            check_split_url_length(&repo_data, 2);
+            check_split_url_length(&repo_data, 3);
 
-            let user_name: &UserName = repo_data[0];
-            let repo_name: &RepoName = repo_data[1];
-            check_repo_data(user_name, repo_name);
+            let host_name: &HostName = repo_data[0];
+            let user_name: &UserName = repo_data[1];
+            let repo_name: &RepoName = repo_data[2];
+            check_repo_data(host_name, user_name, repo_name);
 
-            (user_name, repo_name)
+            (host_name, user_name, repo_name)
         } else {
             panic!()
         }
     }
 
     fn build_github_url(user_name: &str, repo_name: &str, branch_name: &str) -> String {
-        let mut zip_url = String::from("https://codeload.github.com");
-        zip_url.push_str(format!("/{}", user_name).as_str());
-        zip_url.push_str(format!("/{}", repo_name).as_str());
-        zip_url.push_str(format!("/zip/refs/heads/{}", branch_name).as_str());
-        zip_url
+        let mut git_zip_url = String::from("https://codeload.github.com");
+        git_zip_url.push_str(format!("/{}", user_name).as_str());
+        git_zip_url.push_str(format!("/{}", repo_name).as_str());
+        git_zip_url.push_str(format!("/zip/refs/heads/{}", branch_name).as_str());
+        git_zip_url
     }
 
-    pub fn new(url: &'a str, optional_branch_name: Option<&'a String>) -> Self {
-        let (user_name, repo_name) = Self::deserialize_input_url(url);
+    /// A gitlab repo is hosted on gitlab.com or self-hosted.
+    fn build_gitlab_url(
+        host_name: &str,
+        user_name: &str,
+        repo_name: &str,
+        branch_name: &str,
+    ) -> String {
+        format!(
+            "https://{}/{}/{}/-/archive/{}/{}-{}.zip",
+            host_name, user_name, repo_name, branch_name, repo_name, branch_name
+        )
+    }
 
-        let branch_name = match optional_branch_name {
-            Some(branch) => branch,
-            None => "main",
-        };
+    fn build_url(
+        host_name: &HostName,
+        user_name: &UserName,
+        repo_name: &RepoName,
+        branch_name: &str,
+    ) -> String {
+        if host_name.starts_with(GITHUB_HOST) {
+            Self::build_github_url(user_name, repo_name, branch_name)
+        } else {
+            Self::build_gitlab_url(host_name, user_name, repo_name, branch_name)
+        }
+    }
 
-        let zip_url = Self::build_github_url(user_name, repo_name, branch_name);
+    fn request_url(git_zip_url: &str) {
+        panic::set_hook(Box::new(|_| {
+            println!("Could not resolve host !");
+        }));
 
         let response: Vec<u8> = Vec::new();
         let mut easy = Easy2::new(CollectorMemFile(response));
         easy.get(true).unwrap();
-        easy.url(&zip_url).unwrap();
+        easy.url(git_zip_url).unwrap();
         easy.perform().unwrap();
+        let _ = panic::take_hook();
 
         check_url_availability(easy.response_code().unwrap());
+    }
+
+    fn handle_hostname(host_name: &HostName) -> &str {
+        if host_name == "gitlab" {
+            "gitlab.com"
+        } else {
+            host_name
+        }
+    }
+
+    pub fn new(url: &'a str, optional_branch_name: Option<&'a String>) -> Self {
+        let (host_name, user_name, repo_name) = Self::deserialize_input_url(url);
+
+        let branch_name = match optional_branch_name {
+            Some(branch) => branch,
+            None => {
+                if host_name.starts_with(GITHUB_HOST) {
+                    "main"
+                } else {
+                    "master"
+                }
+            }
+        };
+
+        let host_name = Self::handle_hostname(host_name);
+
+        let git_zip_url = Self::build_url(host_name, user_name, repo_name, branch_name);
+        println!("Zip url : {}", git_zip_url);
+
+        Self::request_url(&git_zip_url);
 
         Self {
-            zip_url,
+            git_zip_url,
             repo_name,
             branch_name,
         }
@@ -181,7 +233,7 @@ impl<'a> Git<'a> {
         let mut easy = Easy2::new(CollectorMemFile(memfile));
 
         easy.get(true).unwrap();
-        easy.url(&self.zip_url).unwrap();
+        easy.url(&self.git_zip_url).unwrap();
         easy.perform().unwrap();
         assert_eq!(easy.response_code().unwrap(), 200);
 
@@ -201,7 +253,6 @@ impl<'a> Git<'a> {
             None => self.branch_name,
         };
 
-        //println!("extracting to {}", file_name);
         zip_archive.extract(file_name).unwrap();
 
         let mut options = CopyOptions::new();
@@ -210,7 +261,6 @@ impl<'a> Git<'a> {
 
         let source_dir = format!("{}/{}-{}", file_name, self.repo_name, self.branch_name);
 
-        //println!("Moving dir from {} to {}", source_dir, file_name);
         move_dir(source_dir, "tmp", &options).unwrap();
         fs::remove_dir_all(file_name).unwrap();
         fs::rename("tmp", file_name).unwrap();
@@ -296,6 +346,8 @@ mod tests {
     use super::*;
 
     const GITHUB_URL: &str = "https://codeload.github.com/shadawck/rust-trend/zip/refs/heads/main";
+    const GITLAB_URL: &str = "";
+    const CUSTOM_GITLAB_URL: &str = "";
 
     #[test]
     fn with_full_repo_url_when_deserialize_input_url_then_construct_valid_url() {
@@ -303,7 +355,7 @@ mod tests {
         let optional_branch_name = "main".to_string();
         let git = Git::new(&full_repo_url, Some(&optional_branch_name));
 
-        assert_eq!(git.zip_url, GITHUB_URL)
+        assert_eq!(git.git_zip_url, GITHUB_URL)
     }
 
     #[test]
@@ -312,16 +364,16 @@ mod tests {
         let optional_branch_name = "main".to_string();
         let git = Git::new(&url_without_scheme, Some(&optional_branch_name));
 
-        assert_eq!(git.zip_url, GITHUB_URL)
+        assert_eq!(git.git_zip_url, GITHUB_URL)
     }
 
     #[test]
     fn with_repo_data_when_deserialize_input_url_then_construct_valid_url() {
-        let repo_data = "shadawck:rust-trend".to_string();
+        let repo_data = "github:shadawck:rust-trend".to_string();
         let optional_branch_name = "main".to_string();
         let git = Git::new(&repo_data, Some(&optional_branch_name));
 
-        assert_eq!(git.zip_url, GITHUB_URL)
+        assert_eq!(git.git_zip_url, GITHUB_URL)
     }
 
     #[test]
